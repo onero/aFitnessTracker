1a11278c73b7e88c3a3432e2aca6149a63d81821
Adam Hansen <adamlars90@gmail.com>
2020-01-20 16:29:25 +0100

    Added prod firebase env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDtkRA5jdtz_Yl7VrmtixSar2LIkj-GwhM',
+    authDomain: 'adamino-fitnesstracker.firebaseapp.com',
+    databaseURL: 'https://adamino-fitnesstracker.firebaseio.com',
+    projectId: 'adamino-fitnesstracker',
+    storageBucket: 'adamino-fitnesstracker.appspot.com',
+    messagingSenderId: '373197419680',
+    appId: '1:373197419680:web:e11215c7a79836ac0ec5c5',
+    measurementId: 'G-JX15DZMNCB'
+  }
 };
