56a17759bb246392f2a0de12c1478bdc8473346d
Adam Hansen <adamlars90@gmail.com>
2020-01-22 09:33:38 +0100

    Fixed ViewDestroyed error
    Added onDestroy handling om unsubscribe from ngxs

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,6 +1,8 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { Store } from '@ngxs/store';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Exercise } from '../exercise.model';
 import { TrainingState } from '../state/training.state';
 
@@ -9,24 +11,33 @@ import { TrainingState } from '../state/training.state';
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.scss']
 })
-export class PastTrainingComponent implements AfterViewInit {
+export class PastTrainingComponent implements AfterViewInit, OnDestroy {
   @ViewChild(MatSort, { static: false }) sort: MatSort;
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
-
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
+
+  private unsubscribe$ = new Subject<void>();
 
   constructor(private store: Store, private cd: ChangeDetectorRef) { }
 
   ngAfterViewInit() {
     this.store.select(TrainingState.finishedExercises)
+      // Since this view will be updated, when a new value emits from firestore, we need to handle the subscription for ngxs!
+      .pipe(takeUntil(this.unsubscribe$))
       .subscribe(exercises => {
         this.dataSource.data = exercises;
         this.dataSource.sort = this.sort;
         this.dataSource.paginator = this.paginator;
         // Enforcing change detection for possible reload/relogin
+        // Read more here: https://indepth.dev/everything-you-need-to-know-about-the-expressionchangedafterithasbeencheckederror-error/
         this.cd.detectChanges();
       });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   onFiltering(filterValue: string) {
